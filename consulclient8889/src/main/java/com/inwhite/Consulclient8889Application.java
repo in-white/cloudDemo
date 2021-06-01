46ca9f427a72a8a31cf9e579fd617e0e94c91dee
EncounterU <914935621@qq.com>
2021-06-02 02:32:44 +0800

    Initial commit

---
+++ consulclient8889/src/main/java/com/inwhite/Consulclient8889Application.java
@@ -0,0 +1,13 @@
+package com.inwhite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consulclient8889Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Consulclient8889Application.class, args);
+    }
+
+}
