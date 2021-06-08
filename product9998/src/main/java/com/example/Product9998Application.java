c0d8fa08e14b5ae23f637b9c95aba83ca6c9918e
EncounterU <914935621@qq.com>
2021-06-08 23:52:19 +0800

    更新

---
+++ product9998/src/main/java/com/example/Product9998Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Product9998Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Product9998Application.class, args);
+    }
+
+}
---
+++ users9999/src/main/java/com/inwhite/Users9999Application.java
@@ -0,0 +1,13 @@
+package com.inwhite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Users9999Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Users9999Application.class, args);
+    }
+
+}
